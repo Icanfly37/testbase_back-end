c135090c55d6ab378518f8a93ce62926acc11815
Icanfly37 <101133532+Icanfly37@users.noreply.github.com>
2023-09-14 16:00:34 +0700

    Updated

--- backend/ExcelEngine.py
+++ backend/ExcelEngine.py
@@ -20,17 +20,17 @@ class Excel():
         return self.excel.sheetnames
     def getrows(self):
         sheet = self.excel[self.excel.sheetnames[0]]
-        num_rows = sheet.max_row
-        num_cols = sheet.max_column
+        num_rows = sheet.max_row # จำนวนสมาชิก row สูงสุดใน sheet
+        num_cols = sheet.max_column # จำนวนสมาชิก column สูงสุดใน sheet
         for i in range(1,num_rows+1):
-            self.rows.update({"row "+str(i):each_row(sheet,i,num_rows,num_cols)})
+            self.rows.update({"row "+str(i):each_row(sheet,i,num_rows,num_cols)}) #each_row send list datatype and collect in self.rows dict
         return self.rows
     def getcolumns(self):
         sheet = self.excel[self.excel.sheetnames[0]]
-        num_rows = sheet.max_row
-        num_cols = sheet.max_column
+        num_rows = sheet.max_row # จำนวนสมาชิก row สูงสุดใน sheet
+        num_cols = sheet.max_column # จำนวนสมาชิก column สูงสุดใน sheet
         for i in range(1, num_cols+1):
-            self.cols.update({"col"+str(i):each_column(sheet,i,num_cols,num_rows)})
+            self.cols.update({"col"+str(i):each_column(sheet,i,num_cols,num_rows)}) #each_column send list datatype and collect in self.cols dict
         return self.cols
     def closefile(self):
         self.excel.close()

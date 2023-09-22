e2225bb8cf45dd9a2b5cd4ff5a368fcb3bf3123f
Icanfly37 <101133532+Icanfly37@users.noreply.github.com>
2023-09-23 04:39:18 +0700

    Updated
    รวมโค้ดให้สามารถอ่านไฟล์ excel แล้วเขียนลง firebase และ json ได้แล้ว

--- backend/work.py
+++ backend/work.py
@@ -17,23 +17,26 @@ def OnJson(path,target):
     jsoner.write_json_file(target)
     jsoner.closefile("w")
     
-# def OnDB_C(cred,collection_name,target):
-#     db = Database(cred)
-#     db.get_db()
-#     db.get_collection(collection_name)
-#     id = 1
-#     for i in target:
-#         db.create_doc(i,id)
-#         id += 1
-#     db.close_db()
+def OnDB_C(cred,collection_name,target):
+    db = Database(cred)
+    db.get_db()
+    db.get_collection(collection_name)
+    id = 1
+    for i in target:
+        db.create_doc(i,str(id))
+        id += 1
+    db.close_db()
     
 
-def OnExcel(file):
+def OnExcel(file,db_collection=None):
     ExcelOP = Excel(BytesIO(file))
+    #ExcelOP = Excel(file)
     ExcelOP.openfile()
     rows = ExcelOP.getrows() #for input to database from excel
     OnJson("data.json",rows)
     ExcelOP.closefile()
+    if db_collection is not None:
+        OnDB_C("D:/testbase_back-end/backend/database/serviceAccountKey.json",db_collection,rows)
     return rows
 
 # path = "C:/Users/icanfly37/Desktop/excel_tester/หลักสูตร.xlsx"
